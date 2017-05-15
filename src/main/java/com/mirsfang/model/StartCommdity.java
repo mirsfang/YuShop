817cb5e1b8c2ff517d7283e85bd21ae08816e5c9
mirsfang <mirsfang@163.com>
2017-05-15 18:06:08 +0800

    修改首页

---
+++ src/main/java/com/mirsfang/model/StartCommdity.java
@@ -0,0 +1,24 @@
+package com.mirsfang.model;/**
+ * Created by MirsFang on 2017/5/15.
+ */
+
+import javax.persistence.*;
+
+/***
+ *作者：MirsFang    
+ *模式：       
+ *时间：2017/05/15/下午12:54  
+ *备注  明星单品
+ ***/
+@Entity
+public class StartCommdity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private int id;
+
+    //商品id
+    private int commdity_id;
+
+    private String  image;
+}

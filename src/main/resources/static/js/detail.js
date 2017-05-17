73b5bfd536d1d374b1806a28d05b8b229681c1c7
mirsfang@163.com <109340*a>
2017-05-17 23:20:56 +0800

    写详情和登录

---
+++ src/main/resources/static/js/detail.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Administrator on 2017/5/17.
+ */
+$(document).ready(function(){
+
+    // 开始写 jQuery 代码...
+    console.log("哈哈哈哈");
+
+   $("#btn_next").click(function () {
+       alert("下单成功");
+   });
+});
